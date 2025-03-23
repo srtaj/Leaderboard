e3fadc6c76f235d76f6024d67dc5b5b6cfe2aa2e
Aman Jaiswal <srtaj456@gmail.com>
2025-03-23 19:32:30 +0530

    Resolved Pagination Issue

--- leaderboard/pagination.py
+++
@@ -1,8 +0,0 @@
-# leaderboard/pagination.py
-from rest_framework.pagination import PageNumberPagination
-
-class LeaderboardPagination(PageNumberPagination):
-    page_size = 10
-    max_page_size = 100
-    page_query_param = 'page'
-    page_size_query_param = 'page_size'
